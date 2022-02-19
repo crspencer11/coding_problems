e6c16e004cf8fd450df528ff7cbdbab728312545
crspencer11 <87241853+crspencer11@users.noreply.github.com>
2022-02-19 12:29:17 -0800

    Create is_palindrome.py

---
+++ Leetcode/is_palindrome.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        Checks whether a string is a palindrome or not.
+        
+        :type x: int
+        :rtype: bool
+        """
+        if x == 0:
+            return True
+        elif x < 0 or x % 10 == 0:
+            return False
+        else:
+            rev = 0
+            while rev < x:
+                y = x % 10
+                x //= 10
+                rev = (rev * 10) + y
+        if x == rev or x == rev // 10:
+            return True
+        return False
+            
+        
+            
