755a5184d9abba1eaa1bbc5093dd65bed89ab769
crspencer11 <87241853+crspencer11@users.noreply.github.com>
2022-02-19 12:09:02 -0800

    Create first_bad_version.py

---
+++ Leetcode/first_bad_version.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        Finds first instance of a bad version and returns it.
+        
+        :type n: int
+        :rtype: int
+        """
+        x = 0
+        y = n
+        while x < y:
+            mid = (x + y) // 2
+            if isBadVersion(mid):
+                if not isBadVersion(mid - 1):
+                    return mid
+                else:
+                    y = mid - 1
+            else:
+                x = mid + 1
+        return y
+        
