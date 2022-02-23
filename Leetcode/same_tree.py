afe52b3efc6d09601327a6e7acc0f45f3f60a340
crspencer11 <87241853+crspencer11@users.noreply.github.com>
2022-02-23 13:54:20 -0800

    Create same_tree.py

---
+++ Leetcode/same_tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+        
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None and q is None:
+            return True
+        elif p is None or q is None:
+            return False
+        while p and q:
+            if p.val != q.val:
+                return False
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return True
